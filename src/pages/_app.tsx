f4c8c5364c87715a38f103973003e90ef932fd79
Takshak Mudgal <114814639+takshakmudgal@users.noreply.github.com>
2023-05-05 23:06:48 +0530

    fixed build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-// pages/_app.js
+import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
